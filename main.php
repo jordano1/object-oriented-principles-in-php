f3c7d76e4e3d663b5245e94b044b73944615d1b8
jordano1 <jordanolson1@gmail.com>
2021-03-13 16:32:14 -0700

    added some functionality, can remove specific member and empty the array. made it so you can create instances of a class using start

--- main.php
+++ main.php
@@ -12,10 +12,10 @@ class Team
         $this->members = $members;
     }
 
-    //  global, be cautious
-    public static function start($name, $members = [])
+    //  global, be cautious (... variadic parameter so we don't need to repeat what's in the construct)
+    public static function start(... $params)
     {
-        return new static($name, $members);
+        return new static(... $params);
     }
     public function name()
     {
@@ -34,18 +34,51 @@ class Team
         $this->members[] = $name;
     }
 
-    public function cancel()
+    public function removeMember($name)
     {
-        
+        if(in_array($name, $this->members))
+        {
+            echo("<br>found ".$name."!<br>");
+            
+            unset($this->members[array_search($name, $this->members)]);
+            print_r($this->members);
+        }else{
+            echo("<pre>");
+            echo "<br>could not find ".$name." in ";
+            print_r($this->members);
+            echo("</pre>");
+        }
     }
-    
+
+    public function emptyMember()
+    {
+        echo('<br>emptied!<br>');
+        return $this->members = $members = [];
+    }
+        
+}
+class Member
+{
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function lastViewed()
+    {
+
+    }
 }
 $acme = Team::start('Acme', [
-    'john',
-    'james',
+    new Member('Jordan'),
+    new Member('Jane'),
 ]);
+$warner = Team::start('warner');
+print_r($warner);
+$warner->add();
 $acme->add('jordan');
-$acme->add('jane');
 var_dump($acme->members());
 
 ?>
