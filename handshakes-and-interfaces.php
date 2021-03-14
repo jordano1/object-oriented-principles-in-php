2907406bdd6b3cc6fb0a6bd6473e7d37a2ad5cb7
jordano1 <jordanolson1@gmail.com>
2021-03-14 12:55:38 -0600

    finishing up handshakes / interfaces, interfaces contain methods that are used within multiple classes, must use implements, and any class implementing interface must use the interface behavior

--- handshakes-and-interfaces.php
+++ handshakes-and-interfaces.php
@@ -1,4 +1,39 @@
 <?php
 
+// interfaces need to be used in every class they're used in if they implement
 
+interface Newsletter
+{
+    public function subscribe($email);
 
+}
+
+class CampaignMonitor implements Newsletter
+{
+
+    public function subscribe($email)
+    {
+        die('subscribing with campaign moniter');
+    }
+}
+
+class Drip
+{
+    public function subscribe($email)
+    {
+        die('subscribing with Drip');
+    }
+}
+
+class NewsletterSubscriptionsController
+{
+    public function store(Newsletter $newsletter)
+    {
+        $email = 'foo@foomail.com';
+        $newsletter->subscribe($email);
+    }
+}
+
+$controller = new NewsletterSubscriptionsController();
+
+$controller->store(new Drip());
