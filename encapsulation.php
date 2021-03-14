51da87514e26ce03429acfc5479ab5c030f5825a
jordano1 <jordanolson1@gmail.com>
2021-03-14 13:31:32 -0600

    adding encapsulation section

---
+++ encapsulation.php
@@ -0,0 +1,30 @@
+<?php
+
+class TennisMatch
+{
+
+    public function score()
+    {
+        // winner
+        // advantage
+        // deuce
+    }
+
+    
+    // this class has access 
+    private function hasWinner()
+    {
+        # code...
+    }
+
+    // this class and children can access method
+    protected function hasAdvantage()
+    {
+        # code...
+    }
+    
+    protected function inDeuce()
+    {
+        # code...
+    }
+}
